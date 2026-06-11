67d45cfaec74075df4dcdda105fc42cf4a3ac5d9
Ruan vitor <ruan_wall@estudante.sesisenai.org.br>
2026-06-11 20:12:22 -0300

    questão 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("idade do visitante!");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 16)
+{
+    Console.WriteLine("entrada permitida somente com respónsavel");
+}
+else
+{
+    Console.WriteLine("entrada liberada");
+}
