39867ad1a60ee038bbb848bcdd7cafb3273a97f2
Ruan vitor <ruan_wall@estudante.sesisenai.org.br>
2026-06-15 19:44:44 -0300

    questão 4

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("temperatura atual em graus celsius: "); 
-float temperatura = float.Parse(Console.ReadLine());
-if (temperatura < 18) 
-{ 
-    Console.WriteLine("ambiente frio"); 
-} 
-else if (temperatura >= 18 && temperatura <= 26) 
-{ 
-    Console.WriteLine("ambiente confortável."); 
-} 
-else 
-{ 
-    Console.WriteLine("ambiente quente"); 
+﻿using System.Globalization;
+
+Console.Write("Informe o valor total da compra: ");
+decimal valorCompra = decimal.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+
+decimal desconto = 0;
+
+if (valorCompra >= 200)
+{
+    desconto = valorCompra * 0.10m;
 }
+
+decimal valorFinal = valorCompra - desconto;
+
+Console.WriteLine($"Valor original: R$ {valorCompra:F2}");
+Console.WriteLine($"Desconto aplicado: R$ {desconto:F2}");
+Console.WriteLine($"Valor final: R$ {valorFinal:F2}");
