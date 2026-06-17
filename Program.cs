87b2e163154731c711874cab7dab0833bfa40848
Ruan vitor <ruan_wall@estudante.sesisenai.org.br>
2026-06-17 20:42:30 -0300

    q9

--- Program.cs
+++ Program.cs
@@ -1,28 +1,11 @@
-﻿Console.Write("Informe a primeira pontuação: ");
-int pontuacao1 = int.Parse(Console.ReadLine()!);
+﻿Console.Write("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
 
-Console.Write("Informe a segunda pontuação: ");
-int pontuacao2 = int.Parse(Console.ReadLine()!);
-
-Console.Write("Informe a terceira pontuação: ");
-int pontuacao3 = int.Parse(Console.ReadLine()!);
-
-if (pontuacao1 == pontuacao2 && pontuacao2 == pontuacao3)
+if (numero % 2 == 0)
 {
-    Console.WriteLine("As três pontuações são iguais.");
+    Console.WriteLine("O número informado é par.");
 }
 else
-{}
-    int maiorPontuacao = pontuacao1;
-
-    if (pontuacao2 > maiorPontuacao)
-    {
-        maiorPontuacao = pontuacao2;
-    }
-
-    if (pontuacao3 > maiorPontuacao)
-    {
-        maiorPontuacao = pontuacao3;
-    }
-
-    Console.WriteLine($"Maior pontuação: {maiorPontuacao}");
+{
+    Console.WriteLine("O número informado é ímpar.");
+}
