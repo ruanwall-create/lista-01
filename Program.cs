cd92506d16d37b6fdd75a5c1476c6315715c1c2b
Ruan vitor <ruan_wall@estudante.sesisenai.org.br>
2026-06-17 21:18:00 -0300

    questão 18

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
-﻿decimal nota;
+﻿Console.Write("Informe a quantidade de clientes atendidos: ");
+int quantidade = int.Parse(Console.ReadLine()!);
 
-do
+int tempoTotal = 0;
+
+for (int i = 1; i <= quantidade; i++)
 {
-    Console.Write("Informe uma nota de 0 a 10: ");
-    nota = decimal.Parse(Console.ReadLine()!);
+    Console.Write("Informe o tempo do atendimento: ");
+    int tempo = int.Parse(Console.ReadLine()!);
 
-    if (nota < 0 || nota > 10)
-    {
-        Console.WriteLine("Nota inválida. Informe novamente.");
-    }
+    tempoTotal += tempo;
+}
 
-} while (nota < 0 || nota > 10);
+int media = tempoTotal / quantidade;
 
-Console.WriteLine($"Nota registrada: {nota}");
+Console.WriteLine($"Tempo total de atendimento: {tempoTotal} minutos");
+Console.WriteLine($"Tempo médio por cliente: {media} minutos");
