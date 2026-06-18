c71fcd540344ab98e8a6ed55aec42374f8cbfa36
Ruan vitor <ruan_wall@estudante.sesisenai.org.br>
2026-06-17 21:27:55 -0300

    questão 20

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Informe o saldo inicial: ");
-decimal saldo = decimal.Parse(Console.ReadLine()!);
+﻿Console.Write("Informe a quantidade inicial em estoque: ");
+int estoque = int.Parse(Console.ReadLine()!);
 
 int opcao;
 
@@ -10,28 +10,32 @@ do
 
     if (opcao == 1)
     {
-        Console.Write("Informe o valor da movimentação: ");
-        decimal valor = decimal.Parse(Console.ReadLine()!);
+        Console.Write("Informe a quantidade movimentada: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
 
-        saldo += valor;
-        Console.WriteLine($"Entrada registrada. Saldo atual: R$ {saldo}");
+        estoque += quantidade;
+        Console.WriteLine($"Entrada registrada. Estoque atual: {estoque}");
     }
     else if (opcao == 2)
     {
-        Console.Write("Informe o valor da movimentação: ");
-        decimal valor = decimal.Parse(Console.ReadLine()!);
+        Console.Write("Informe a quantidade movimentada: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
 
-        if (valor <= saldo)
+        if (quantidade <= estoque)
         {
-            saldo -= valor;
-            Console.WriteLine($"Saída registrada. Saldo atual: R$ {saldo}");
+            estoque -= quantidade;
+            Console.WriteLine($"Saída registrada. Estoque atual: {estoque}");
         }
         else
         {
-            Console.WriteLine("Saldo insuficiente.");
+            Console.WriteLine("Quantidade insuficiente em estoque.");
         }
     }
+    else if (opcao == 3)
+    {
+        Console.WriteLine($"Estoque atual: {estoque}");
+    }
 
-} while (opcao != 3);
+} while (opcao != 4);
 
-Console.WriteLine($"Saldo final: R$ {saldo}");
+Console.WriteLine($"Estoque final: {estoque}");
